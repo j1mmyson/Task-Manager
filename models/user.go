666cfb0b0944b1d14ef1deb7476e5491fe66f8f3
j1mmyson <bwson98@gmail.com>
2021-07-08 16:26:58 +0900

    add user related function

--- models/user.go
+++ models/user.go
@@ -1,11 +1,22 @@
 package models
 
 import (
+	"time"
+
 	_ "gorm.io/gorm"
 )
 
 type User struct {
-	ID       string `json:"id" gorm:"primary_key"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
+	ID        string    `json:"id" gorm:"primary_key"`
+	Password  string    `json:"password"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func GetUserFromUserId(uid string) *User {
+	var u User
+
+	DB.First(&u, "id = ?", uid)
+
+	return &u
 }
