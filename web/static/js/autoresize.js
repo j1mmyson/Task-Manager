74461d65e0ce9ec6381c848e708e6756c63928d4
j1mmyson <bwson98@gmail.com>
2021-07-17 00:46:17 +0900

    card 크기 자동 조정

---
+++ web/static/js/autoresize.js
@@ -0,0 +1,12 @@
+// const content = document.querySelector(".content");
+
+
+
+function resize(obj) {
+    // console.log("resize()!")
+    obj.style.height = "auto";
+    obj.style.height = obj.scrollHeight;
+}
+
+// content.addEventListener("keydown", resize);
+// content.addEventListener("keyup", resize);
