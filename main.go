b1c3410268c42cd9e337418171d2fded56b9702b
j1mmyson <bwson98@gmail.com>
2021-07-19 21:46:12 +0900

    embed templates & static files

--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
+	"embed"
+	"html/template"
+	"net/http"
+	"path"
+
 	"github.com/gin-gonic/gin"
 	"github.com/j1mmyson/reviewList/controller"
 	"github.com/j1mmyson/reviewList/models"
 )
 
+var (
+	//go:embed web/templates/*
+	templatesFS embed.FS
+
+	//go:embed web
+	staticFS embed.FS
+)
+
 func main() {
+
 	r := gin.Default()
+	LoadHTMLFromEmbedFS(r, templatesFS, "web/templates/*")
+
+	r.GET("/static/*filepath", func(c *gin.Context) {
+		c.FileFromFS(path.Join("/web/", c.Request.URL.Path), http.FS(staticFS))
+	})
 
 	models.ConnectDB()
-
-	r.LoadHTMLGlob("web/templates/*")
-	r.Static("/web/static", "./web/static")
 
 	r.GET("/", controller.LogInPage)
 	r.POST("/", controller.LogIn)
@@ -27,5 +43,11 @@ func main() {
 	r.POST("/delete/:id", controller.DeleteListById)
 	r.POST("/edit/:id", controller.EditListById)
 
-	r.Run()
+	r.Run("")
+}
+
+func LoadHTMLFromEmbedFS(r *gin.Engine, em embed.FS, pattern string) {
+	templ := template.Must(template.ParseFS(em, pattern))
+
+	r.SetHTMLTemplate(templ)
 }
