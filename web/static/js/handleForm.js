67e86794bb2f769b8a2ea4f1618501fab24dfb9d
j1mmyson <bwson98@gmail.com>
2021-08-23 21:28:00 +0900

    ADD cancel +add card form

--- web/static/js/handleForm.js
+++ web/static/js/handleForm.js
@@ -7,20 +7,27 @@ doneAdd = document.getElementById("doneAddCard");
 inprogressAdd = document.getElementById("inProgressAddCard");
 todoAdd = document.getElementById("toDoAddCard");
 
+doneCancel = document.getElementById("doneCancel");
+inprogressCancel = document.getElementById("inprogressCancel");
+todoCancel = document.getElementById("todoCancel");
+
 
 function handleForm(event) {
     switch (event.target.id){
         case "doneAddCard":
+        case "doneCancel":
             doneForm.classList.toggle('invisible');
-            event.target.classList.toggle('invisible');
+            document.getElementById("doneAddCard").classList.toggle('invisible');
             break
         case "inProgressAddCard":
+        case "inprogressCancel":
             inprogressForm.classList.toggle('invisible');
-            event.target.classList.toggle('invisible');
+            document.getElementById("inProgressAddCard").classList.toggle('invisible');
             break
         case "toDoAddCard":
+        case "todoCancel":
             todoForm.classList.toggle('invisible');
-            event.target.classList.toggle('invisible');
+            document.getElementById("toDoAddCard").classList.toggle('invisible');
             break
     }
 }
@@ -28,3 +35,7 @@ function handleForm(event) {
 doneAdd.addEventListener('click', handleForm);
 inprogressAdd.addEventListener('click', handleForm);
 todoAdd.addEventListener('click', handleForm);
+
+doneCancel.addEventListener('click', handleForm);
+todoCancel.addEventListener('click', handleForm);
+inprogressCancel.addEventListener('click', handleForm);
